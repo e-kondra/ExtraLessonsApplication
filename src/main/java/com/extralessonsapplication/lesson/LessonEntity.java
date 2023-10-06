4c56eb4d877f63f98deda89e383a43d665e9a771
Valeria Zalygina <lerazalygina@gmail.com>
2023-10-06 13:33:12 +0300

    Lesson

--- src/main/java/com/extralessonsapplication/lesson/LessonEntity.java
+++ src/main/java/com/extralessonsapplication/lesson/LessonEntity.java
@@ -1,4 +1,26 @@
 package com.extralessonsapplication.lesson;
+import com.extralessonsapplication.student.StudentEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class LessonEntity {
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+    private Boolean isActive;
+    @OneToMany (mappedBy = "school")
+    private List<LessonEntity> lessons;
+
+
 }
--- src/main/java/com/extralessonsapplication/lesson/LessonRepository.java
+++ src/main/java/com/extralessonsapplication/lesson/LessonRepository.java
@@ -1,4 +1,10 @@
 package com.extralessonsapplication.lesson;
 
-public class LessonRepository {
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LessonRepository extends CrudRepository<LessonEntity, Long> {
 }
+
