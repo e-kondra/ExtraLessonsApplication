380aeef8617132064b47a80ff1fbef53b1754f11
e-kondra <e-kondra@mail.ru>
2023-10-12 18:31:46 +0300

    teacher's CRUD for Students

--- src/main/java/com/extralessonsapplication/student/StudentRepository.java
+++ src/main/java/com/extralessonsapplication/student/StudentRepository.java
@@ -5,6 +5,7 @@ import com.extralessonsapplication.school.SchoolEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
 import java.util.List;
 
 @Repository
